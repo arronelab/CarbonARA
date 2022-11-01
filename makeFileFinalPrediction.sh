e07d255d667aa24dc93d4dcb2031b0a0424af896
mckeownish <j.mckeown124@gmail.com>
2022-11-01 17:09:43 +0000

    update makefile

--- makeFileFinalPrediction.sh
+++ makeFileFinalPrediction.sh
@@ -1,17 +1,19 @@
 #/bin/sh
-rm finalSrc/*.o
+rm src/*.o
 
 g++ -c -O3 -std=gnu++14 -o src/point.o  src/point.cpp -fopenmp
-g++ -c -O3 -std=gnu++14 -o src/optimizationPoint.o src/optimizationPoint.cpp -fopenmp
+# g++ -c -O3 -std=gnu++14 -o src/optimizationPoint.o src/optimizationPoint.cpp -fopenmp
 g++ -c -O3 -std=gnu++14 -o src/polyHelix.o src/polyHelix.cpp -fopenmp
 g++ -c -O3 -std=gnu++14 -o src/randomMolGen.o src/randomMolGen.cpp -fopenmp
 g++ -c -O3 -std=gnu++14 -o src/ktlMoleculeRandom.o src/ktlMoleculeRandom.cpp -fopenmp
 g++ -c -O3 -std=gnu++14 -o src/experimentalData.o src/experimentalData.cpp -fopenmp
 g++ -c -O3 -std=gnu++14 -o src/hydrationShellRandom.o src/hydrationShellRandom.cpp -fopenmp
-g++ -c -O3 -std=gnu++14 -o src/binaryFind.o src/binaryFind.cpp -fopenmp
-g++ -c -O3 -std=gnu++14 -o src/getSections.o src/getSections.cpp -fopenmp
-g++ -c -O3 -std=gnu++14 -o src/mutualWind2.o src/mutualWind2.cpp -fopenmp
-g++ -c -O3 -std=gnu++14 -o src/localWrithe.o src/localWrithe.cpp -fopenmp
+# g++ -c -O3 -std=gnu++14 -o src/binaryFind.o src/binaryFind.cpp -fopenmp
+# g++ -c -O3 -std=gnu++14 -o src/getSections.o src/getSections.cpp -fopenmp
+# g++ -c -O3 -std=gnu++14 -o src/mutualWind2.o src/mutualWind2.cpp -fopenmp
+# g++ -c -O3 -std=gnu++14 -o src/localWrithe.o src/localWrithe.cpp -fopenmp
+g++ -c -O3 -std=gnu++14 -o src/mainPredictionFinal.o src/writhe.cpp -fopenmp
 g++ -c -O3 -std=gnu++14 -o src/moleculeFitAndState.o src/moleculeFitAndState.cpp -fopenmp
 g++ -c -O3 -std=gnu++14 -o src/mainPredictionFinal.o src/mainPredictionFinal.cpp -fopenmp
-g++ -O3 -std=gnu++14 -o predictStructure src/point.o src/optimizationPoint.o src/polyHelix.o src/randomMolGen.o src/ktlMoleculeRandom.o src/experimentalData.o src/hydrationShellRandom.o src/binaryFind.o src/mutualWind2.o src/localWrithe.o src/moleculeFitAndState.o src/mainPredictionFinal.o -fopenmp
+
+g++ -O3 -std=gnu++14 -o predictStructure src/point.o src/polyHelix.o src/randomMolGen.o src/ktlMoleculeRandom.o src/experimentalData.o src/hydrationShellRandom.o src/writhe.cpp src/moleculeFitAndState.o src/mainPredictionFinal.o -fopenmp
